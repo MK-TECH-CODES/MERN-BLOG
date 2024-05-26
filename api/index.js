8ac05640d5977ce62f9691fbece9449f30686b2e
MK-TECH-CODES <mukeshkumar14000@gmail.com>
2024-05-26 11:07:48 +0530

    Connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+//OKlp61KcizLvveTC
 
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Mongo_db is connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 app.listen(3000, () => {
   console.log(` server is running in 3000`);
 });
