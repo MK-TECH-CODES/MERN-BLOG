538d36cc03421ef0cad5beae5bea9c8dac0e9fe1
MK-TECH-CODES <mukeshkumar14000@gmail.com>
2024-06-02 21:44:26 +0530

    sidebar of the dashboard

---
+++ client/src/components/Dashprofile.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Dashprofile() {
+  return <div>Dashprofile</div>;
+}
--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Dashsidebar from "../components/dashsidebar";
+import Dashprofile from "../components/Dashprofile";
 
 export default function dashboard() {
-  return <div>dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    // console.log(tabFromUrl);
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/*Sidebar */}
+        <Dashsidebar />
+      </div>
+      {/* Profile */}
+      {tab === "profile" && <Dashprofile />}
+    </div>
+  );
 }
